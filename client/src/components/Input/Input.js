8933ef444fb3697e5e0c13061faab84908520122
shashwatshaurya <shashwatshaurya.98@gmail.com>
2020-05-10 02:49:40 +0530

    added all folders

---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import closeIcon from '../../icons/closeIcon.png';
+import onlineIcon from '../../icons/onlineIcon.png';
+
+import './Input.css';
+
+const Input = ({ message, setMessage, sendMessage }) => (
+    <form className="form">
+        <input 
+            className="input"
+            type="text"
+            placeholder="Type a message..."
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyPress={(event) => event.key === 'Enter'? sendMessage(event):null} 
+        />
+        <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+    </form>
+)
+
+export default Input;
---
+++ server/README.md
@@ -0,0 +1,1 @@
+run npm i and npm start for both server and client side.
